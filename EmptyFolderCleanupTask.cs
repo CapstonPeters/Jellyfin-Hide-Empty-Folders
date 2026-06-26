56e1989195d296718d6d4413328cf3eb1c02b6a0
Jo <jo@local>
2026-06-26 16:20:04 +0000

    Fix: add Jellyfin.Data.Enums and MediaBrowser.Model.Entities usings for BaseItemKind

--- EmptyFolderCleanupTask.cs
+++ EmptyFolderCleanupTask.cs
@@ -1,5 +1,7 @@
+using Jellyfin.Data.Enums;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Tasks;
 using Microsoft.Extensions.Logging;
 using Jellyfin.Plugin.HideEmptyFolders.Configuration;
